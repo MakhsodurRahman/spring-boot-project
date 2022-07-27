96c486699d2c5b7846c1b26d73ef34456aa4d1bf
MakhsdourRahman <makhsodurrahmanhridoy@gmial.com>
2022-07-27 11:17:31 +0600

    change path name @RepositoryRestResource(path = "members")

--- spring-project/src/main/java/com/makhsodur/springproject/dao/EmployeeRepository.java
+++ spring-project/src/main/java/com/makhsodur/springproject/dao/EmployeeRepository.java
@@ -3,9 +3,11 @@ package com.makhsodur.springproject.dao;
 import com.makhsodur.springproject.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import javax.persistence.criteria.CriteriaBuilder;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
